6dc77cbf74874d1b39b7f64491d5c0af7afdb77c
nhok210724 <nhok210724@gmail.com>
2022-05-23 12:21:34 +0700

    fix port

--- function/server_web.js
+++ function/server_web.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const server = ()=>{
+
     app.get("/riot.txt",(req,res)=>{
-        res.render(process.env.KEY_RIOT);
+        res.send(process.env.KEY_RIOT);
     });
     app.listen(PORT,()=>console.log("App listen on PORT= "+PORT));
 }
