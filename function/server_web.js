853135132bfb6f7e31d73f2f1198516c540a10f7
nhok210724 <nhok210724@gmail.com>
2022-05-23 11:39:03 +0700

    change port

--- function/server_web.js
+++ function/server_web.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 //express sever
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = ()=>{
     app.get("/riot.txt",(req,res)=>{
         res.render(process.env.KEY_RIOT);
     });
-    app.listen(PORT,()=>console.log("App listen on PORT="+PORT));
+    app.listen(PORT,()=>console.log("App listen on PORT= "+PORT));
 }
 
 module.exports={
